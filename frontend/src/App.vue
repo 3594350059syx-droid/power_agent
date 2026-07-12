632358a9039c9961575290f6cc96d3c92374c1c4
StarKing729 <1293238723@qq.com>
2026-07-12 15:15:06 +0800

    完成脚手架纯净清理：移除业务页面、路由、侧边栏菜单

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,7 @@
 <template>
   <el-container style="height: 100vh;">
     <el-aside width="200px" style="background-color: rgb(238, 241, 246);">
-      <el-menu router default-active="/chat">
-        <el-menu-item index="/chat">AI对话诊断</el-menu-item>
-        <el-menu-item index="/monitor">实时监控面板</el-menu-item>
+      <el-menu router>
       </el-menu>
     </el-aside>
     <el-main>
@@ -16,8 +14,6 @@
 </script>
 
 <style>
-* {
-  margin: 0;
-  padding: 0;
-}
+margin: 0;
+padding: 0;
 </style>
