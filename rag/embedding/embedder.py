111774627383f3de7b6506218fa59f563a1dfb66
tttyyyggg <757491554@qq.com>
2026-07-15 17:12:26 +0800

    feat: RAG模块 - 实现文档切片(chunker)和Embedding(embedder)

---
+++ rag/embedding/embedder.py
@@ -0,0 +1,26 @@
+from sentence_transformers import SentenceTransformer
+
+
+class Embedder:
+
+    def __init__(self):
+
+        # 中文语义模型
+        self.model = SentenceTransformer(
+            "shibing624/text2vec-base-chinese"
+        )
+
+
+    def encode(self, texts):
+
+        embeddings = self.model.encode(
+            texts,
+            normalize_embeddings=True,
+            show_progress_bar=True
+        )
+
+        return embeddings.tolist()
+
+
+
+embedder = Embedder()
